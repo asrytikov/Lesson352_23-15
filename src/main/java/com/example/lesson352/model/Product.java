42a8165a6ea2af60089ef59ba6f9a4b4cc029f3a
asrytikov <asrytikov@gmail.com>
2023-11-28 11:40:22 +0400

    Lesson35

--- src/main/java/com/example/lesson352/model/Product.java
+++ src/main/java/com/example/lesson352/model/Product.java
@@ -1,2 +1,28 @@
-package com.example.lesson352.model;public class Product {
+package com.example.lesson352.model;
+
+public class Product {
+
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- src/main/java/com/example/lesson352/service/ProductService.java
+++ src/main/java/com/example/lesson352/service/ProductService.java
@@ -1,2 +1,22 @@
-package com.example.lesson352.service;public class ProductService {
+package com.example.lesson352.service;
+
+import com.example.lesson352.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+    public List<Product> findAll(){
+        return products;
+    }
+
 }
